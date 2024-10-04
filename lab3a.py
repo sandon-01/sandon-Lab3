383e3ca64774acb13a73c8b31e8ea53b99155450
Shermalyn Andon <izhameandon@gmail.com>
2024-10-04 18:18:16 -0400

    Completed lab3a

--- lab3a.py
+++ lab3a.py
@@ -1,3 +1,26 @@
-def hello():
-    print('Hello World')
-    print('Inside a Function')
+#!/usr/bin/env python3
+
+# return_text_value() function
+# Author ID:160564217
+
+def return_text_value():
+    name = 'Terry'
+    greeting = 'Good Morning ' + name
+    return greeting
+
+# return_number_value() function
+
+def return_number_value():
+    num1 = 10
+    num2 = 5
+    num3 = num1 + num2
+    return num3
+
+# Main Program
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
