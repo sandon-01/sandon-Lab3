9dcbb17c88d565312154edaf93d6f6700daf7896
Shermalyn Andon <izhameandon@gmail.com>
2024-10-04 22:24:57 -0400

    Completed test_import

---
+++ test_import.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import lab3a
+import lab3b
+import lab3c
+import lab3d
+import lab3e
+from lab3f import *
+
+text = lab3a.return_text_value()
+print(text)
+print(lab3a.return_number_value())
+
+
+print(lab3b.sum_numbers(10, 5))
+print(lab3b.subtract_numbers(10, 5))
+print(lab3b.multiply_numbers(10, 5))
